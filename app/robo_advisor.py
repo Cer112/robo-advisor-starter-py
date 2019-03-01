4944fb1022e71c3025ec74080a60fdc2624d4f0a
Cer112 <46543271+Cer112@users.noreply.github.com>
2019-03-01 10:22:58 -0500

    opened csv file

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import json
+import csv
 import os
+
 import requests
 
 def to_usd(my_price):
@@ -14,24 +16,12 @@ api_key = os.environ.get("ALPHAVANTAGE_API_KEY")
 
 symbol = "NFLX" # TODO: capture user input, like... input("Please specify a stock symbol: ")
 
-# see: https://www.alphavantage.co/documentation/#daily (or a different endpoint, as desired)
-# TODO: assemble the request url to get daily data for the given stock symbol...
-
-# TODO: use the "requests" package to issue a "GET" request to the specified url, and store the JSON response in a variable...
-
-# TODO: further parse the JSON response...
-
-# TODO: traverse the nested response data structure to find the latest closing price and other values of interest...
-
-
-
 #
 # INFO OUTPUTS
 #
 
 
 request_url = "http://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
-
 
 response = requests.get(request_url)
 
@@ -54,13 +44,10 @@ for date in dates:
     high_price = TSD[date]["2. high"]
     low_price = TSD[date]["3. low"]
     high_prices.append(float(high_price))
-    low_prices.append(float(low_price)
+    low_prices.append(float(low_price))
 
-
-recent_high = max(high_prices)
-recent_low = min(low_prices)
-
-# TODO: write response data to a CSV file
+latest_high = max(high_prices)
+latest_low = min(low_prices)
 
 # TODO: further revise the example outputs below to reflect real information
 
@@ -68,11 +55,28 @@ print("-----------------")
 print(f"STOCK SYMBOL: {symbol}")
 print("RUN AT: 11:52pm on June 5th, 2018") #use datetime module
 print("-----------------")
-print(f"LATEST DAY OF AVAILABLE DATA: {last_refreshed}")
-print(f"LATEST DAILY CLOSING PRICE: {to_usd(float(latest_close))}")
-print("RECENT AVERAGE HIGH CLOSING PRICE: {to_usd(float(recent_high))}")
-print("RECENT AVERAGE LOW CLOSING PRICE: {to_usd(float(recent_low))}")
+print(f"LATEST DAY OF AVAILABLE DATA: {last_refreshed}") #fix
+print(f"LATEST DAILY CLOSING PRICE: {to_usd(float(latest_close))}") #fix
+print("RECENT AVERAGE HIGH CLOSING PRICE: {to_usd(float(latest_high))}")
+print("RECENT AVERAGE LOW CLOSING PRICE: {to_usd(float(latest_low))}")
 print("-----------------")
 print("RECOMMENDATION: Buy!")
 print("RECOMMENDATION REASON: Because the latest closing price is within threshold XYZ etc., etc. and this fits within your risk tolerance etc., etc.")
 print("-----------------")
+print("WRITING CSV")
+print("-----------------")
+print("HAPPY INVESTING")
+print("-----------------")
+
+csv_file_path = "prices.csv"
+
+#csv_file_path = os.path.join(os.path.direname(__file__), "..", "data", "prices.csv")
+
+with open(csv_file_path, "w") as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    writer.writeheader()
+    writer.writerow({"city": "New York", "name": "Yankees"})
+    writer.writerow({"city": "New York", "name": "Mets"})
+    writer.writerow({"city": "Boston", "name": "Red Sox"})
+    writer.writerow({"city": "New Haven", "name": "Ravens"})
+
